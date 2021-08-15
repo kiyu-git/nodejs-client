db5eb6fb8093a97b10730243baf7f0ea13ea4371
Nishida Kiyu <kiyu@kiyu-mac.local>
2021-08-15 14:23:25 +0900

    first commit

---
+++ client.js
@@ -0,0 +1,12 @@
+const { Client, Server } = require('node-osc');
+const io = require('socket.io-client');
+
+// OSCで使用するポートを指定
+const client = new Client('127.0.0.1', 3333);
+
+var socket = io.connect('');//接続先のサーバを指定
+
+socket.on('message' ,function (data) {
+  client.send('/message', String(data));
+  console.log(String(data))
+});
